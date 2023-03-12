9f7688af9971d312bf3f2e008ef37cac1a76e519
Артём Полторацкий <kakashihotakimail.ru@gmail.com>
2023-03-12 20:58:00 +0300

    Fix bad code

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRouter from "./components/appRouter/AppRouter.jsx";
 import Navbar from "./components/navbar/Navbar.jsx";
-import Footer from "./components/footer/Footer.jsx";
+import Footer from "./components/footer/footer.jsx";
 import "./Style.scss";
 
 const App = (props) => {
